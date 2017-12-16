6f2eb6eb93c4af074bad9980e650d0e76678980e
Adam Gedney <adam.gedney@gmail.com>
2017-12-15 21:54:45 -0500

    ADDED coinmarketcap data to details routes

--- src/app/components/asset-detail/asset-detail.component.ts
+++ src/app/components/asset-detail/asset-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+const CoinMarketCap = require('node-coinmarketcap');
+const coinmarketcap = new CoinMarketCap();
+
 
 @Component({
   selector: 'app-asset-detail',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asset-detail.component.scss']
 })
 export class AssetDetailComponent implements OnInit {
+  assetId = '';
+  asset = {};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    const self = this;
+
+    this.route.params.subscribe(params => {
+      self.assetId = params.assetName;
+
+      coinmarketcap.get(self.assetId, coin => {
+        self.asset = coin;
+        console.log('COIN',coin)
+      });
+    });
+  }
 
   ngOnInit() {
+    console.log('ASSET DETAILS', this);
+  }
+
+  colorIndicateMovement(value){
+    const isNegative = value.indexOf('-') > -1;
+
+    if(isNegative){
+      return `asset-detail--negative`;
+    }else{
+      return `asset-detail--positive`;
+    }
   }
 
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+const CoinMarketCap = require('node-coinmarketcap');
+var options = {
+  events: true, // Enable event system
+  refresh: 3, // Refresh time in seconds (Default: 60)
+  convert: "USD" // Convert price to different currencies. (Default USD)
+}
+const coinmarketcap = new CoinMarketCap(options);
+
+// coinmarketcap.get("bitcoin", coin => {
+//   console.log(coin.price_usd); // Prints the price in USD of BTC at the moment.
+// });
+
+// coinmarketcap.on("BTC", (coin) => {
+//   console.log(coin, Math.random());
+// });
 
 const coins = [
   {
@@ -15,12 +30,19 @@ const coins = [
 export class SidebarComponent implements OnInit {
   constructor() { }
 
-  coins: any = coins;
+  assetList: any = [];
 
   ngOnInit() {
+    const self = this;
+
+    coinmarketcap.multi(coins => {
+      self.assetList = coins
+      .getTop(10)
+      .sort((a,b) =>  a.rank - b.rank);
+    });
   }
 
-  onAssetClick(asset){
+  onAssetClick(asset) {
     console.log(asset);
   }
 
